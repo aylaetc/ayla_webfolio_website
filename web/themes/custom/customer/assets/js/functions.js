c994ccdad175e173fd17968e0a4159063eaf24fa
Elegna <contact@angelecottet.com>
2020-07-18 14:27:35 +0200

    Ajsutements A

--- web/themes/custom/customer/assets/js/functions.js
+++ web/themes/custom/customer/assets/js/functions.js
@@ -2,7 +2,7 @@
 //     // disable slider on small viewport
 //     disable: true,
 //     responsive: {
-//       1200: {
+//       1280: {
 //         // enable slider on big viewport
 //         disable: false,
 //         container: '.slider',
